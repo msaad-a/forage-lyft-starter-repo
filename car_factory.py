75aef10147d35f3af50c7e52d128506a50b7d13b
msaad-a <msaad24@outlook.com>
2023-03-11 21:01:47 -0500

    code refactor

---
+++ car_factory.py
@@ -0,0 +1,34 @@
+from battery import NubbinBattery, SpindlerBattery
+from car import Car
+from engine import CapuletEngine, SternmanEngine, WilloughbyEngine
+
+class CarFactory:
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+
+    def create_palindrome(current_date, last_service_date, warning_light_on):
+        engine = SternmanEngine(warning_light_on)
+        battery = SpindlerBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
---
+++ engine.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+from car import Car
+
+class Engine(Car, ABC):
+    @abstractmethod
+    def needs_service(self):
+        pass
